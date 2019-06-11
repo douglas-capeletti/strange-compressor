14d8629815d37a842289d486b2006d028d8cdfd7
17103446 <17103446@facin.portoalegre.pucrsnet.br>
2019-06-10 22:12:33 -0300

    correcoes na leitura de dados

--- src/main.c
+++ src/main.c
@@ -2,56 +2,52 @@
 #include <stdlib.h>
 #include <strings.h>
 
-int main()
+void leituraIndexada(char *arquivo, int *freqs);
+void imprimeDados(int *freqs);
+
+int main(int argc, char **argv)
 {
-	int freqs[256];
-	for(int i=0; i<256; i++){
-		freqs[i]=0;
-	}
+    int dados[256];
+    leituraIndexada(argv[1], dados);
 
-	// FILE *arq = fopen(argv[1], "r");
-	FILE *arq = fopen("exemplos.data", "r");
+    for (int i = 0; i < 256; i++)
+    {
+        printf("Char: %c %d \n", i, dados[i]);
+    }
 
-	if (arq == NULL)
-	{
-		printf("Erro! Arquivo não encontrado!\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	char c;
-	while ((c = fgetc(arq)) != EOF)
-		freqs[c]++;
-	printf("char: %c\n", c);
-
-	for (int i=0; i<256; i++){
-		printf("%c %d", i, freqs[i]);
-	}
-
-   rewind(arq); // volta ao início do arquivo
-
-    char aux[200];
-    while(fgets(aux, 200, arq) != NULL)
-        printf("str: [%s]\n", aux);
-
-    rewind(arq); // volta ao início do arquivo
-
-    while(fscanf(arq, "%s", aux) != EOF)
-        printf("palavra: %s\n", aux);
-
-    rewind(arq); // volta ao início do arquivo
-
-    int num;
-    fscanf(arq, "%d", &num); // tenta ler o no.
-    printf("Número: %d\n", num);
-
-    fgets(aux, 200, arq); // pula o \n do 42
-    fgets(aux, 200, arq); // 2a. linha
-    printf("linha 1: %s", aux);
-    fgets(aux, 200, arq); // 3a. linha
-    printf("linha 2: %s", aux);
-
-
-
-    fclose(arq);
     return 0;
+}
+
+void leituraIndexada(char *arquivo, int *freqs)
+{
+    //leitura
+    FILE *arq = fopen(arquivo, "r");
+    if (arq == NULL)
+    {
+        printf("Erro! Arquivo não encontrado!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //zera os valores nas posicoes
+    for (int i = 0; i < 256; i++)
+    {
+        freqs[i] = 0;
+    }
+
+    //leitura de caracter armazenando na posicao relativa da tabela ascii
+    char c;
+    while ((c = fgetc(arq)) != EOF)
+    {
+        freqs[c]++;
+    }
+    fclose(arq);
+    return freqs;
+}
+
+void imprimeDados(int *freqs)
+{
+    for (int i = 0; i < 256; i++)
+    {
+        printf("Char: %c %d \n", i, freqs[i]);
+    }
 }
